df766a960667773b00add78579e7ec1d71d44d03
suryaravikumar-pixer <suryaravikumar@gmail.com>
2021-07-12 19:50:08 +0530

    To style the register from crispy forms created

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     #local apps
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+
+    # 3rd party apps
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
