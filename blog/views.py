39491204a804ffbfe92a7cecf01aa4f6962bc645
suryaravikumar-pixer <suryaravikumar@gmail.com>
2021-07-12 13:55:32 +0530

    Post database model created, model is migrated to hted to th to the databse.

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author':'AynRand',
-        'title':'Fountainhead',
-        'content':'The Fountainhead tells the story of the desperate battle waged by architect Howard Roark.',
-        'date_posted':'July 10, 2021',
-    },
-    {
-        'author':'Howard',
-        'title':'Atlas',
-        'content':'Second post content ',
-        'date_posted':'July 10, 2021',
-    }
-]
+
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     """The created dictionary context passed as third argument to the render function,
     by doing it will pass our data into our template. """
